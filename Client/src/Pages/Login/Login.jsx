319e2448cd3e5593d476ca49da3b8e977f925aa8
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-06-10 03:03:36 +0100

    dispatch created for login start

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,8 +1,23 @@
 import './Login.css'
+import { Context } from '../../Component/context/Context';
 import {Link } from "react-router-dom"
 
 
 const Login = () => {
+  const userRef =  useRef();
+  const passwordRef =  useREf()
+
+  const { dispatch, isFetching } = useContext(Context)
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault(); 
+
+
+      // this is used to call the login_start written in context js
+      // when button is clicked 'isFecthing' is set to true
+      dispatch({})
+    }
   return (
     <>
     
@@ -16,26 +31,47 @@ const Login = () => {
 
                     </div>
 
-            <form action="">
+            <form action="" onSubmit={handleSubmit}>
               {
                 // <label> Username </label>
               }
-                  <input type="text" class="input--register" placeholder="Email / Username" />
+                  <input 
+                          type="text" 
+                          class="input--register" 
+                          placeholder="Email / Username" 
+
+                          // refrence for username login 
+                          ref={userRef}
+                          />
                   {
                     // <label> Password </label>
                   }
-                  <input type="text" class="input--register" placeholder="Password" />
-                  <p class="forgot">Forgot Password ?</p>
+                  <input 
+                          type="text" 
+                          class="input--register" 
+                          placeholder="Password" 
+
+                          // refrence for Password login 
+                          ref={passwordRef}
+                          />
+
+
+                  <p 
+                      class="forgot">
+                      Forgot Password ?  
+                  </p>
             </form>
 
             <div class="submit--div">
-            <button class="sign-btn"> 
-                  <Link to="/admin-page" className='link'>
-                      Sign In
-                    </Link>
-            </button>
-            { // <span> This page is only for admin</span> 
-             }
+                    <button 
+                              class="sign-btn"> 
+                          <Link to="/admin-page" 
+                                className='link'>
+                                  Sign In
+                            </Link>
+                    </button>
+                      { // <span> This page is only for admin</span> 
+                      }
             </div>
 
  
