48ccc24dbceb5e234eedf0a679091b04dddf4b13
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-06-15 10:11:48 +0100

    update on App

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,19 @@
-import {CreativeWrite, Post, About, Login, Tech} from './Component/Index'
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
-
-
+import {CreativeWrite, Post, About, Login, Edit} from './Component/Index'
+import { BrowserRouter, Routes, Route,  useNavigate} from "react-router-dom";
+import { Context } from './Component/context/Context';
+import { useContext  } from 'react'
 function App() {
-  const user =  false;
-
+  const {user} =  useContext(Context);
+      
   return (
    
     <BrowserRouter >   
         <Routes> 
         <Route exact path='/' element={<About />} />
         <Route  path='/posts/' element={<CreativeWrite />} /> 
-        <Route  path='/posts/:Id' element={<Post />} /> 
-        {
-          //  <Route  path='/posts/:Id' element={<Edit />} /> 
-        }
-       
-
+        <Route  path='/posts/:Id' element={<Post />} />
         <Route  path='/admin-login' element={<Login />} /> 
+     
 
        </Routes>
     </BrowserRouter>
