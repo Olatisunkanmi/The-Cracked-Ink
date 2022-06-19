cdcfabc68c1c6367ce8ec87563f62241b066101e
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-06-19 09:09:22 +0100

    Admin page 🤞

--- Client/src/Pages/EditPost/Edit.jsx
+++ Client/src/Pages/EditPost/Edit.jsx
@@ -1,23 +1,37 @@
 import './Edit.css';
-import { Facebook, NotificationImportantOutlined } from '@material-ui/icons';
+import { Book, ExitToApp, Logout, NotificationImportantOutlined, Settings } from '@material-ui/icons';
 
 const Edit = () => {
   return (
       <>
-            <div className="topar--container">
-                    <div className="left--tp--div"> Writer Control Panel</div>
-                    <div className="right--tp--div">
-                          <ul className="right--ul">
+            <div className="topbar--container">
+                      <div className="wrapper">
+                            <div className="left--tp--div">
+                                <Book className='book'/> 
+                                  <p className='write'> Writer Control Panel</p>
+                                </div>
 
-                                <li className="right--item"> 
-                                    <img src="" alt="" />
-                                </li>
-                                <li className="right--item"> 
-                                      <Facebook />
-                                </li>
+                          
+                                  <div className="right--tp--div">
+                                        <ul className="right--ul">
 
-                          </ul>
-                    </div>
+                                              <li className="right--item"> 
+                                                  <img src="" alt="" />
+                                              </li>
+                                              <li className="right--item"> 
+                                                  <NotificationImportantOutlined />
+                                              </li>
+                                              <li className="right--item"> 
+                                                  <Settings />
+                                              </li>
+                                              <li className="right--item"> 
+                                                  <ExitToApp />
+                                                  
+                                              </li>
+
+                                        </ul>
+                                  </div>
+                      </div>
             </div>
       </>
   )
