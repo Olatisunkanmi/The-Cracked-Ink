fc0365be4584246e7da9ceadfe61bbfa6ebd7a1b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-06-22 07:42:54 +0100

    Hooks tiny changes

--- Client/src/Hooks/RequireAuth.jsx
+++ Client/src/Hooks/RequireAuth.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useState } from 'react';
 import { useAuth } from './useAuth';
+import axios from 'axios';
 import {Outlet, Navigate , useLocation  } from 'react-router-dom'
+import { useEffect } from 'react';
 
 
 const RequireAuth = () => {
-    const {auth , user } = useAuth();
-    console.log(user);
+    const { user } = useAuth();
     const location = useLocation();
+    
 
+   
     return (
         user ? <Outlet /> : <Navigate to='/admin-login' state={{ from : location}} replace />
     )
--- Client/src/Hooks/useAuth.jsx
+++ Client/src/Hooks/useAuth.jsx
@@ -2,7 +2,6 @@ import { Context } from '../Component/context/Context';
 
 import { useContext } from 'react';
 
-import React from 'react'
 
 export const useAuth = () => {
   return  useContext(Context)
