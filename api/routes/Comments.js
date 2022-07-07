12d852dcda76b91b6640959b187b97ae8d0053ff
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-07 02:16:34 +0100

    Update Comments.js

--- api/routes/Comments.js
+++ api/routes/Comments.js
@@ -9,28 +9,23 @@ const router = express.Router();
 
 router.put('/:id',  async (req, res)   => {
     const   {username, comment} = req.body;
-
+       
             try {
                 const post = await PostDB.findById(req.params.id);
                 // res.status(201).json(post)
                        if(post){
-                        try {
-                            // Get the index of tthe arrays in the posts comments 
-                            const CommentSection =    post.comments;
-                             CommentSection.push(comment)
-                            const newComment = await CommentSection.save()
-                            
-                            console.log(CommentSection)
-                        } catch (error) {
-                                
-                            res.status(404).json(error)
-                        } 
+                                    try {
+                                        // Get the index of tthe arrays in the posts comments 
+                                        const CommentSection =    post.comments;
+                                        CommentSection.push(comment)
+                                        const newComment = await post.save()
 
+                                        res.status(201).json(newComment)
+                                    } catch (error) {
+                                            
+                                        res.status(404).json(error)
+                                    } 
                        }
-                        else{
-                        console.log('Post not found ')
-                       }
-
         }catch (error) {
                     res.status(404).json(error)
                 }
