f93d33e9a517fe5ae0098f46ad629d1e9598973a
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-12 01:12:31 +0100

    Update EachPosts.jsx

--- Client/src/Component/EachPost/EachPosts.jsx
+++ Client/src/Component/EachPost/EachPosts.jsx
@@ -1,4 +1,5 @@
-import { Topbar, Header, Footer } from "../Index"
+import { Topbar, Header, Footer } from "../Index";
+import './Each.css'
 
 
 const Posts = () => {
@@ -6,9 +7,60 @@ const Posts = () => {
     <div>
         <Header />
         <Topbar  /> 
-        <div>
-          <h3>Each Posts to be rendered </h3>
+
+
+
+
+
+
+
+
+
+        <div className="fond">
+          <span className="si"></span>
+          <span className="s2"> Card</span>
+      
+      
+             <div className="card">
+                  <div className="thumbnail">
+                    <img src='' alt="" />
+                  </div>
+             </div>
+
         </div>
+     
+      
+          <div className="right">
+
+            <h1>Why you Need More Magnesium in Your Daily Diet</h1>
+                  <div className="author">
+                  <img />
+                <h2>IGOY MArty</h2>
+                </div>
+
+
+                <div className="separator">
+                        <p> 
+                        Magnesium is one of the six essential macro-minerals that is required by the body for energy production and synthesis of protein and enzymes. It contributes to the development of bones and most importantly it is responsible for synthesis of your DNA and RNA. A new report that has appeared in theBritish Journal of Cancer, gives you another reason to add more magnesium to your diet...
+                      </p> 
+                </div>
+
+          </div>
+      
+      
+            <h5>12</h5>
+        <h6>January</h6>
+        
+
+
+
+
+
+        
+
+
+
+        
         <Footer /> 
     </div>
   )
