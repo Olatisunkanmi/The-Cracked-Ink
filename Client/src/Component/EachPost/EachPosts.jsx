4e842cee2b984598eb030aba00ccaa6f0d9d34d3
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-12 10:09:39 +0100

    Update EachPosts.jsx

--- Client/src/Component/EachPost/EachPosts.jsx
+++ Client/src/Component/EachPost/EachPosts.jsx
@@ -1,5 +1,8 @@
 import { Topbar, Header, Footer } from "../Index";
+
 import './Each.css'
+import img from '../../Assests/pg.jpg'
+import { ArrowRightAlt, CommentOutlined, FavoriteBorderOutlined, Share } from "@material-ui/icons";
 
 
 const Posts = () => {
@@ -8,48 +11,52 @@ const Posts = () => {
         <Header />
         <Topbar  /> 
 
+        <div className="flex flex-col md:flex-row justify-around text-center p-4" >
+            
+              <div 
+                className="m-5 mb-2 md:w-1/2 ">
+                          <div 
+                            className="bg-white p-5 lg:px-40 relative text-left">
+                                              <h3
+                                              className="font-serif text-left capitalize text-2xl tracking-wider font-semibold"> Some kind Of Title </h3>
+                                              
+                                              <div className="mb-7 h-1 bg-zinc-700 border-none w-8"></div>
+                                            <p
+                                              className="para">
+                                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nam sequi modi possimus natus. Alias, ipsum quisquam, voluptatem quasi pariatur ullam commodi, officia consequatur ipsa eius sequi quae? Mollitia, totam!
+                                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nam sequi modi possimus natus. Alias, ipsum quisquam, voluptatem quasi pariatur ullam commodi, officia consequatur ipsa eius sequi quae? Mollitia, totam!
+                                              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe nam sequi modi possimus natus. Alias, ipsum quisquam, voluptatem quasi pariatur ullam commodi, officia consequatur ipsa eius sequi quae? Mollitia, totam!
+                                            </p>
+                        
+                                            <div  
+                                            className="mt-5">
+                                                    <a href="" className="">
+                                                    READ MORE
+                                                   <ArrowRightAlt 
+                                                    className=""/>
+                                                  </a>
+                                                   
+                                                  <div className=" h-1 bg-zinc-700 border-none w-8"></div>
+
+                                                <div  
+                                                  className="space-x-5 mt-5"> 
+                                                <Share />
+                                                <FavoriteBorderOutlined />
+                                                  <span> 39 <CommentOutlined />  </span>
+                                                </div>
+
+                                            </div>
+                          </div>
+              </div>
 
 
+              <div 
+                className="w-1/2">
+                    Hello
+              </div>
 
-
-
-
-
-
-        <div className="fond">
-          <span className="si"></span>
-          <span className="s2"> Card</span>
+       </div>
       
-      
-             <div className="card">
-                  <div className="thumbnail">
-                    <img src='' alt="" />
-                  </div>
-             </div>
-
-        </div>
-     
-      
-          <div className="right">
-
-            <h1>Why you Need More Magnesium in Your Daily Diet</h1>
-                  <div className="author">
-                  <img />
-                <h2>IGOY MArty</h2>
-                </div>
-
-
-                <div className="separator">
-                        <p> 
-                        Magnesium is one of the six essential macro-minerals that is required by the body for energy production and synthesis of protein and enzymes. It contributes to the development of bones and most importantly it is responsible for synthesis of your DNA and RNA. A new report that has appeared in theBritish Journal of Cancer, gives you another reason to add more magnesium to your diet...
-                      </p> 
-                </div>
-
-          </div>
-      
-      
-            <h5>12</h5>
-        <h6>January</h6>
         
 
 
