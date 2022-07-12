2b4e9ac47e4b7055ac6d1ce518224b3127688ee7
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-12 14:44:39 +0100

    Update Index.js

--- Client/src/Component/Index.js
+++ Client/src/Component/Index.js
@@ -1,16 +1,16 @@
-export {default as Topbar } from './Topbar/Topbar';
-export { default as Header } from './Header/Header';
+export {default as Topbar } from './Topbar';
+export { default as Header } from './Header';
 export {default as CreativeWrite} from '../Pages/CreativeWrite/CreativeWrite';
-export {default as Footer } from './Footer/Footer';
+export {default as Footer } from './Footer';
 export {default as CardList} from './CardList/CardList';
 export { default as FeaturedCard} from './FeaturedCard/FeaturedCard';
 export {default as FirstCard } from './FirstCard/FirstCard';
-export {default as SinglePost } from './SinglePost/SinglePost';
+export {default as SinglePost } from './SinglePost';
 export { default as Login } from '../Pages/Login/Login';
 export { default as About } from '../Pages/About/About';
 export {default as Post} from '../Pages/Post/Post';
-export {default as Tech} from '../Pages/Tech/Tech';
 export { default as Edit} from '../Pages/EditPost/Edit';
 export { default as NotFound} from '../Pages/404/NotFound';
 export {default as Write} from '../Pages/Write/Write';
-export { default as EachPosts } from './EachPost/EachPosts';
+export { default as EachPosts } from './EachPost/EachPostsPage';
+export {default as AllPostsCard } from './AllPostsCard'
