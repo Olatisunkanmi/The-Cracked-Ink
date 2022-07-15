26daa0be792bca99e6d64802181fde30116bda10
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-15 22:49:11 +0100

    Update EachPostsPage.jsx

--- Client/src/Component/EachPost/EachPostsPage.jsx
+++ Client/src/Component/EachPost/EachPostsPage.jsx
@@ -1,9 +1,7 @@
 import { Topbar, Header, Footer,  AllPostsCard} from "../Index";
 import axios from "axios"
 import { useEffect , useState } from "react";
-import './Each.css'
-import img from '../../Assests/pg.jpg'
-import { ArrowRightAlt, CommentOutlined, FavoriteBorderOutlined, Share } from "@material-ui/icons";
+
 
 
 const Posts = () => {
