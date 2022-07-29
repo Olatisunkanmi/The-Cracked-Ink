b749d85a2470eeeb276345474e80667adc35a0bc
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-30 00:49:52 +0100

    Complete

--- api/index.js
+++ api/index.js
@@ -1,52 +1,24 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const { response } = require('express');
-const AUTHROUTE = require("./routes/auth");
-const USERLOGIN = require("./routes/users");
-const POSTCREATE = require("./routes/Posts")
-const CATCREATE = require("./routes/Categories")
-const COMCREATE = require("./routes/Comments")
-const multer = require('multer');
-const path = require('path');
-const cors = require('cors');
-
-
+const express = require("express");
 const app = express();
-
-app.use(
-    cors({
-       origin: "http://localhost:3000",
-       optionsSuccessStatus: 200
-})
-
-)
-
-
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname,"/images")))
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
-// Connect to mongodb in the .env files.
-const t1 = new Date();
-
-const main = async () => {
-  await mongoose.connect(process.env.MONGO_URL, {
-    
+mongoose
+  .connect(process.env.MONGO_URL, {
+   
   })
-
-  .then(console.log('Mongo'))
-
-
-}
-main() .catch(err => console.log(err));
-const t2 = new Date();
-console.log(`${t2-t1} MSecs`);
-
-
-// this fn is to store the images uploaded from the 'req.body'
-// destination: saves file to image folder
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -62,20 +34,11 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-
-
-
-// .then(response, () => {console.log('Mongo Works')})
-// .catch(err =>  console.log(`Not working ${err}`) )
-
-// Creating all users Routes 
-
-app.use("/api/auth", AUTHROUTE); 
-app.use("/api/login", USERLOGIN); 
-app.use("/api/posts", POSTCREATE);  
-app.use("/api/category", CATCREATE); 
-app.use("/api/comment", COMCREATE); 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
-    console.log('Node is listening on Port 5000');
-})
+  console.log("Backend is running.");
+});
