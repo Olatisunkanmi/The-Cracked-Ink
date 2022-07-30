f126541f48e947fd2721589b522a3312f0994878
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-07-30 10:06:22 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-const port = process.env.port || 5000;
+const port = process.env.port || 3500;
 
 app.listen(port, () => {
     console.log(`Node is listening on Port ${port}`);
