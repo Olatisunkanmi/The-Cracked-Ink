5f5ba396c7109531add7a51975f6bb4339d0d19b
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-03 15:00:51 +0100

    REFACTOR

--- api/index.js
+++ api/index.js
@@ -1,49 +1,31 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-const authRoute = require("./routes/auth");
-const userRoute = require("./routes/Users");
-const postRoute = require("./routes/Posts");
-const categoryRoute = require("./routes/Categories");
-const multer = require("multer");
-const path = require("path");
-var cors = require('cors')
+const morgan = require('morgan');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/Users');
+const POSTROUTE = require('./routes/PostRoute');
+const categoryRoute = require('./routes/Categories');
+const multer = require('multer');
+const path = require('path');
 
-dotenv.config();
-
-app.use(cors())
 app.use(express.json());
-app.use("/images", express.static(path.join(__dirname, "/images")));
-
-mongoose
-  .connect(process.env.MONGO_URL, {
-   
-  })
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+app.use(morgan('common'));
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  res.status(200).json("File has been uploaded");
+app.post('/api/upload', upload.single('file'), (req, res) => {
+	res.status(200).json('File has been uploaded');
 });
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/categories", categoryRoute);
+app.use('/api/v2/posts', POSTROUTE);
 
-const port = (process.env.PORT || 3500);
-
-app.listen(port, () => {
-    console.log(`Node is listening on Port ${port}`);
-})
+module.exports = app;
