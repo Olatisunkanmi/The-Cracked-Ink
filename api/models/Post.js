217f4bcf6a8b3c282c1813d12d45753d02d1173f
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-04 10:54:59 +0100

    Update Post.js

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,41 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const PostSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    desc: {
-      type: String,
-      required: true,
-    },
-    photo: {
-      type: String,
-      required: false,
-    },
-    
-    username: {
-      type: String,
-      required: false,
-    },
-    categories: {
-      type: Array,
-      required: false,
-    },
-    comments: {
-            type: Array,
-            required: false,
-    },
+	{
+		title: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		desc: {
+			type: String,
+			required: true,
+		},
+		photo: {
+			type: String,
+			required: false,
+		},
 
-    likes: {
-      type: Array,
-      required : false ,
-    }
-    
-  },
-  { timestamps: true }
+		username: {
+			type: String,
+			required: false,
+		},
+		categories: {
+			type: Array,
+			required: [true, 'Post must have a category'],
+		},
+		comments: {
+			type: Array,
+			required: false,
+		},
+
+		likes: {
+			type: Array,
+			required: false,
+		},
+	},
+	{ timestamps: true },
 );
 
-module.exports = mongoose.model("Post", PostSchema);
+module.exports = mongoose.model('Post', PostSchema);
