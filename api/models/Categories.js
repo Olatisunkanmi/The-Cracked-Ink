5ac830f92456dbe468956ff9a3e38b7ee2c53b61
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-04 10:54:50 +0100

    Update Categories.js

--- api/models/Categories.js
+++ api/models/Categories.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const CategorySchema = new mongoose.Schema ({
-    name:{
-        type:String,
-        required:true,
-        
-    },
-},
-        {timestamps: true}
-)
+const CategorySchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true },
+);
 
-module.exports = mongoose.model("Categories ", CategorySchema);
+module.exports = mongoose.model('Categories ', CategorySchema);
