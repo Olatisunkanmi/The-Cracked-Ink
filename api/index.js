5fba2bf26e69ebbe988f4bc151715224edfa1538
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-13 13:41:31 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/Users');
-const POSTROUTE = require('./routes/PostRoute');
-const categoryRoute = require('./routes/CategoryRoute');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
+const USERROUTE = require('./routes/Users');
+const POSTROUTE = require('./routes/PostRoute');
+const AUTH = require('./routes/auth');
+const app = express();
+app.use(express.json());
 app.use(cors('*'));
 
-app.use(express.json());
 app.use(morgan('common'));
 app.use('/images', express.static(path.join(__dirname, '/images')));
 
@@ -24,10 +23,12 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post('/api/upload', upload.single('file'), (req, res) => {
+app.post('/api/v2/upload', upload.single('file'), (req, res) => {
 	res.status(200).json('File has been uploaded');
 });
 
 app.use('/api/v2/posts', POSTROUTE);
+app.use('/api/v2/users', USERROUTE);
+app.use('/api/v2/auth', AUTH);
 
 module.exports = app;
