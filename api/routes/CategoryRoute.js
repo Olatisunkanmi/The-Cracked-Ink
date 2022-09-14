4a665219cb5ac2fdf3426393b3e39cb8bce5561f
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-09-14 21:23:34 +0100

    Delete CategoryRoute.js

--- api/routes/CategoryRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const CategoryController = require('../controllers/CategoryConntroller');
-
-router
-	.route('/')
-	.get(CategoryController.getCat)
-	.post(CategoryController.createCat);
-
-module.exports = router;
